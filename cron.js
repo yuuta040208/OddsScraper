8ab32acd8006058287aed7922f63f1074198bd2f
evitch <yuuta040208@gmail.com>
2018-08-25 22:35:20 +0900

    crontimeを修正

--- cron.js
+++ cron.js
@@ -9,11 +9,8 @@ const childProcess = require('child_process');
 * メイン処理
 **********************************/
 
-// 土曜日のレースをスクレイピング
-// cronTimeの設定
-const timeSaturday = '0 */30 * * * 6';
-
-// cronOddsInfoの設定
+// cronの設定(土曜日)
+const timeSaturday = '0 0 23 * * 5';
 const cronSaturday = new cronJob({
     //実行したい日時 or crontab書式
     cronTime: timeSaturday
@@ -21,11 +18,6 @@ const cronSaturday = new cronJob({
     //指定時に実行したい関数
     , onTick: function() {
         childProcess.execSync('node scrape_raceinfo.js 0');
-        childProcess.execSync('node scrape_odds.js');
-        childProcess.execSync('node select.js');
-        childProcess.execSync('node insert.js');
-        childProcess.execSync('node score.js');
-        childProcess.execSync('node tweet.js');
 
         console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
     }
@@ -42,29 +34,16 @@ const cronSaturday = new cronJob({
     , timeZone: "Asia/Tokyo"
 })
 
-//ジョブ開始
-//cronRaceInfo.start();
-cronSaturday.start();
 
-// 土曜日のレースをスクレイピング
-// cronTimeの設定
-const timeSunday = '0 */30 * * * 0';
-
-
-// 日曜日のレースをスクレイピング
-// cronOddsInfoの設定
+// cronの設定(日曜日)
+const timeSunday = '0 0 23 * * 6';
 const cronSunday = new cronJob({
     //実行したい日時 or crontab書式
-    cronTime: timeSunday
+    cronTime: timeSaturday
 
     //指定時に実行したい関数
     , onTick: function() {
-        childProcess.execSync('node scrape_raceinfo.js 1');
-        childProcess.execSync('node scrape_odds.js');
-        childProcess.execSync('node select.js');
-        childProcess.execSync('node insert.js');
-        childProcess.execSync('node score.js');
-        childProcess.execSync('node tweet.js');
+        childProcess.execSync('node scrape_raceinfo.js 0');
 
         console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
     }
@@ -81,5 +60,34 @@ const cronSunday = new cronJob({
     , timeZone: "Asia/Tokyo"
 })
 
+
+// cronの設定(土・日曜日)
+const timeWeekend = '0 */30 * * * *';
+const cronWeekend = new cronJob({
+    //実行したい日時 or crontab書式
+    cronTime: timeSaturday
+
+    //指定時に実行したい関数
+    , onTick: function() {
+        childProcess.execSync('node scrape_raceinfo.js 0');
+
+        console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
+    }
+
+    //ジョブの完了または停止時に実行する関数 
+    , onComplete: function() {
+        console.log('onComplete!')
+    }
+
+    // コンストラクタを終する前にジョブを開始するかどうか
+    , start: false
+
+    //タイムゾーン
+    , timeZone: "Asia/Tokyo"
+})
+
+
 //ジョブ開始
+cronSaturday.start();
 cronSunday.start();
+cronEveryday.start();
