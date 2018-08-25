474afd61a1d78cf1e118cf9434b9a2edcbf1f9eb
evitch <yuuta040208@gmail.com>
2018-08-25 22:22:49 +0900

    レース情報を自動的にスクレイピングするよう変更

--- cron.js
+++ cron.js
@@ -9,16 +9,18 @@ const childProcess = require('child_process');
 * メイン処理
 **********************************/
 
+// 土曜日のレースをスクレイピング
 // cronTimeの設定
-const cronOddsInfoTime = '0 */30 * * * *';
+const timeSaturday = '0 */30 * * * 6';
 
 // cronOddsInfoの設定
-const cronOddsInfo = new cronJob({
+const cronSaturday = new cronJob({
     //実行したい日時 or crontab書式
-    cronTime: cronOddsInfoTime
+    cronTime: timeSaturday
 
     //指定時に実行したい関数
     , onTick: function() {
+        childProcess.execSync('node scrape_raceinfo.js 0');
         childProcess.execSync('node scrape_odds.js');
         childProcess.execSync('node select.js');
         childProcess.execSync('node insert.js');
@@ -42,7 +44,42 @@ const cronOddsInfo = new cronJob({
 
 //ジョブ開始
 //cronRaceInfo.start();
-cronOddsInfo.start();
+cronSaturday.start();
 
-//ジョブ停止
-//job.stop();
+// 土曜日のレースをスクレイピング
+// cronTimeの設定
+const timeSunday = '0 */30 * * * 0';
+
+
+// 日曜日のレースをスクレイピング
+// cronOddsInfoの設定
+const cronSunday = new cronJob({
+    //実行したい日時 or crontab書式
+    cronTime: timeSunday
+
+    //指定時に実行したい関数
+    , onTick: function() {
+        childProcess.execSync('node scrape_raceinfo.js 1');
+        childProcess.execSync('node scrape_odds.js');
+        childProcess.execSync('node select.js');
+        childProcess.execSync('node insert.js');
+        childProcess.execSync('node score.js');
+        childProcess.execSync('node tweet.js');
+
+        console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
+    }
+
+    //ジョブの完了または停止時に実行する関数 
+    , onComplete: function() {
+        console.log('onComplete!')
+    }
+
+    // コンストラクタを終する前にジョブを開始するかどうか
+    , start: false
+
+    //タイムゾーン
+    , timeZone: "Asia/Tokyo"
+})
+
+//ジョブ開始
+cronSunday.start();
